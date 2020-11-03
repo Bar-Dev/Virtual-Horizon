ecb5352ed20cb3c6a474a41551e3eb2e6ee75efd
Bar-Dev <bdprojectservices@gmail.com>
2020-11-03 16:56:24 +0000

    Update Pricing layout

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,13 +3,13 @@
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'bdprojectservices@gmail.com';
+    $email_from = 'barry@virtualhorizon.ie';
     $email_subject = "New Form Submission";
     $email_body = "User Name: $name.\n". 
                     "User Email: $visitor_email.\n". 
                         "User Message: $message.\n";
 
-    $to = "bardoyle@icloud.com";
+    $to = "info@virtualhorizon.ie";
     $headers = "From: $email_from \r\n";
     $headers = "Reply-To: $visitor_email \r\n";
     mail($to,$email_subject,$email_body,$headers);
